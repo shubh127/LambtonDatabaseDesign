7f59b2c9dc37ceaec119998008cf87c870595dca
Shubham Behal <shubhambehal@Shubhams-MacBook-Pro.local>
2021-09-16 19:26:09 -0400

    ticket table populated

--- app/src/main/java/com/example/lamtondatabasedesign/activities/TicketInfoActivity.java
+++ app/src/main/java/com/example/lamtondatabasedesign/activities/TicketInfoActivity.java
@@ -3,14 +3,117 @@ package com.example.lamtondatabasedesign.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.lamtondatabasedesign.R;
+import com.example.lamtondatabasedesign.database.DataBaseHelper;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
-public class TicketInfoActivity extends AppCompatActivity {
+import java.util.Objects;
+
+public class TicketInfoActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextInputEditText tietTicketNo;
+    private TextInputEditText tietTicketType;
+    private TextInputEditText tietRefNo;
+    private TextInputEditText tietTicketPrice;
+    private TextInputEditText tietTicketStatus;
+
+    private TextInputLayout tilTicketNo;
+    private TextInputLayout tilTicketType;
+    private TextInputLayout tilRefNo;
+    private TextInputLayout tilTicketPrice;
+    private TextInputLayout tilTicketStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket_info);
+
+        initViews();
     }
+
+    private void initViews() {
+        tietTicketNo = findViewById(R.id.tiet_ticket_no);
+        tietTicketType = findViewById(R.id.tiet_ticket_type);
+        tietRefNo = findViewById(R.id.tiet_ref_no);
+        tietTicketPrice = findViewById(R.id.tiet_price);
+        tietTicketStatus = findViewById(R.id.tiet_status);
+
+        tilTicketNo = findViewById(R.id.til_ticket_no);
+        tilTicketType = findViewById(R.id.til_ticket_type);
+        tilRefNo = findViewById(R.id.til_ref_no);
+        tilTicketPrice = findViewById(R.id.til_price);
+        tilTicketStatus = findViewById(R.id.til_status);
+
+        Button btnSubmit = findViewById(R.id.btn_submit_details);
+
+        btnSubmit.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btn_submit_details && validateDetails()) {
+            saveDataToDatabase();
+        }
+    }
+
+    private boolean validateDetails() {
+        boolean isValid = true;
+
+        if (TextUtils.isEmpty(tietTicketNo.getText())) {
+            tilTicketNo.setError(getString(R.string.empty_error_txt));
+            isValid = false;
+        } else {
+            tilTicketNo.setErrorEnabled(false);
+        }
+
+        if (TextUtils.isEmpty(tietTicketType.getText())) {
+            tilTicketType.setError(getString(R.string.empty_error_txt));
+            isValid = false;
+        } else {
+            tilTicketType.setError(null);
+        }
+
+        if (TextUtils.isEmpty(tietTicketPrice.getText())) {
+            tilTicketPrice.setError(getString(R.string.empty_error_txt));
+            isValid = false;
+        } else {
+            tilTicketPrice.setError(null);
+        }
+
+        if (TextUtils.isEmpty(tietTicketStatus.getText())) {
+            tilTicketStatus.setError(getString(R.string.empty_error_txt));
+            isValid = false;
+        } else {
+            tilTicketStatus.setError(null);
+        }
+
+        if (TextUtils.isEmpty(tietRefNo.getText())) {
+            tilRefNo.setError(getString(R.string.empty_error_txt));
+            isValid = false;
+        } else {
+            tilRefNo.setError(null);
+        }
+
+        return isValid;
+    }
+
+    private void saveDataToDatabase() {
+        DataBaseHelper dbHelper = new DataBaseHelper(this);
+        boolean isSuccess = dbHelper.addTicketInfo(
+                Integer.parseInt(Objects.requireNonNull(tietTicketNo.getText()).toString()),
+                Objects.requireNonNull(tietTicketType.getText()).toString(),
+                Integer.parseInt(Objects.requireNonNull(tietRefNo.getText()).toString()),
+                Integer.parseInt(Objects.requireNonNull(tietTicketPrice.getText()).toString()),
+                Objects.requireNonNull(tietTicketStatus.getText()).toString());
+        dbHelper.close();
+        if (isSuccess) {
+            finish();
+        }
+    }
+
 }
--- app/src/main/java/com/example/lamtondatabasedesign/database/DataBaseHelper.java
+++ app/src/main/java/com/example/lamtondatabasedesign/database/DataBaseHelper.java
@@ -12,20 +12,38 @@ import com.example.lamtondatabasedesign.R;
 
 public class DataBaseHelper extends SQLiteOpenHelper {
 
-    private Context context;
+    private final Context context;
     public static final String DATABASE_NAME = "AirlineTicketReservation.db";
     public static final int DATABASE_VERSION = 1;
 
     public static final String TABLE_NAME_FLIGHT_INFO = "FLIGHT_INFO";
+    public static final String TABLE_NAME_REFERENCE_INFO = "REFERENCE_INFO";
+    public static final String TABLE_NAME_PASSENGER_INFO = "PASSENGER_INFO";
+    public static final String TABLE_NAME_TICKET_INFO = "TICKET_INFO";
 
-    public static final String FLIGHT_COLUMN_FLIGHT_NO = "fNo";
-    public static final String FLIGHT_COLUMN_AIRCRAFT = "fAircraft";
-    public static final String FLIGHT_COLUMN_AIRCRAFT_TYPE = "fAircraftType";
-    public static final String FLIGHT_COLUMN_DESTINATION = "fDestination";
-    public static final String FLIGHT_COLUMN_AIRLINE_CODE = "fAirlineCode";
-    public static final String FLIGHT_COLUMN_TIME = "fTime";
-    public static final String FLIGHT_COLUMN_SOURCE = "fSource";
-    public static final String FLIGHT_COLUMN_REF_NO = "referenceNo";
+    public static final String COLUMN_FLIGHT_NO = "flightNo";
+    public static final String COLUMN_AIRCRAFT = "aircraft";
+    public static final String COLUMN_AIRCRAFT_TYPE = "aircraftType";
+    public static final String COLUMN_DESTINATION = "destination";
+    public static final String COLUMN_AIRLINE_CODE = "AirlineCode";
+    public static final String COLUMN_FLIGHT_TIME = "flightTime";
+    public static final String COLUMN_SOURCE = "source";
+    public static final String COLUMN_REF_NO = "referenceNo";
+    public static final String COLUMN_PHONE = "phone";
+    public static final String COLUMN_TICKET_NO = "ticketNo";
+    public static final String COLUMN_PID = "passengerId";
+    public static final String COLUMN_NAME = "passengerName";
+    public static final String COLUMN_ADDRESS = "passengerAddress";
+    public static final String COLUMN_POSTAL_CODE = "postalCode";
+    public static final String COLUMN_CITY = "city";
+    public static final String COLUMN_PROVINCE = "province";
+    public static final String COLUMN_COUNTRY = "country";
+    public static final String COLUMN_PHONE_NO = "phoneNo";
+    public static final String COLUMN_EMAIL = "email";
+    public static final String COLUMN_DOB = "dob";
+    public static final String COLUMN_TICKET_TYPE = "ticketType";
+    public static final String COLUMN_TICKET_PRICE = "ticketPrice";
+    public static final String COLUMN_TICKET_STATUS = "ticketStatus";
 
 
     public DataBaseHelper(@Nullable Context context) {
@@ -43,14 +61,48 @@ public class DataBaseHelper extends SQLiteOpenHelper {
 
         //creating flight table
         db.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_NAME_FLIGHT_INFO +
-                " (" + FLIGHT_COLUMN_FLIGHT_NO + " INTEGER PRIMARY KEY , " +
-                FLIGHT_COLUMN_AIRCRAFT + " TEXT, " +
-                FLIGHT_COLUMN_AIRCRAFT_TYPE + " TEXT, " +
-                FLIGHT_COLUMN_DESTINATION + " TEXT, " +
-                FLIGHT_COLUMN_AIRLINE_CODE + " TEXT, " +
-                FLIGHT_COLUMN_TIME + " TEXT, " +
-                FLIGHT_COLUMN_SOURCE + " TEXT, " +
-                FLIGHT_COLUMN_REF_NO + " INTEGER);");
+                " (" + COLUMN_FLIGHT_NO + " INTEGER PRIMARY KEY , " +
+                COLUMN_AIRCRAFT + " TEXT, " +
+                COLUMN_AIRCRAFT_TYPE + " TEXT, " +
+                COLUMN_DESTINATION + " TEXT, " +
+                COLUMN_AIRLINE_CODE + " TEXT, " +
+                COLUMN_FLIGHT_TIME + " TEXT, " +
+                COLUMN_SOURCE + " TEXT, " +
+                COLUMN_REF_NO + " INTEGER);");
+
+        //creating reference table
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_NAME_REFERENCE_INFO +
+                " (" + COLUMN_REF_NO + " INTEGER PRIMARY KEY , " +
+                COLUMN_AIRLINE_CODE + " TEXT, " +
+                COLUMN_SOURCE + " TEXT, " +
+                COLUMN_DESTINATION + " TEXT, " +
+                COLUMN_PHONE + " INTEGER, " +
+                COLUMN_TICKET_NO + " INTEGER, " +
+                COLUMN_PID + " INTEGER, " +
+                COLUMN_FLIGHT_NO + " INTEGER);");
+
+        //creating passenger table
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_NAME_PASSENGER_INFO +
+                " (" + COLUMN_PID + " INTEGER PRIMARY KEY , " +
+                COLUMN_NAME + " TEXT, " +
+                COLUMN_ADDRESS + " TEXT, " +
+                COLUMN_POSTAL_CODE + " TEXT, " +
+                COLUMN_CITY + " TEXT, " +
+                COLUMN_PROVINCE + " TEXT, " +
+                COLUMN_COUNTRY + " TEXT, " +
+                COLUMN_PHONE_NO + " INTEGER, " +
+                COLUMN_EMAIL + " TEXT, " +
+                COLUMN_DOB + " TEXT, " +
+                COLUMN_TICKET_NO + " INTEGER, " +
+                COLUMN_REF_NO + " INTEGER);");
+
+        //creating ticket table
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_NAME_TICKET_INFO +
+                " (" + COLUMN_TICKET_NO + " INTEGER PRIMARY KEY , " +
+                COLUMN_TICKET_TYPE + " TEXT, " +
+                COLUMN_REF_NO + " INTEGER, " +
+                COLUMN_TICKET_PRICE + " INTEGER, " +
+                COLUMN_TICKET_STATUS + " TEXT);");
     }
 
     @Override
@@ -62,16 +114,36 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
 
-        values.put(FLIGHT_COLUMN_FLIGHT_NO, flightNo);
-        values.put(FLIGHT_COLUMN_AIRCRAFT, aircraft);
-        values.put(FLIGHT_COLUMN_AIRCRAFT_TYPE, aircraftType);
-        values.put(FLIGHT_COLUMN_DESTINATION, destination);
-        values.put(FLIGHT_COLUMN_AIRLINE_CODE, airlineCode);
-        values.put(FLIGHT_COLUMN_TIME, flightTime);
-        values.put(FLIGHT_COLUMN_SOURCE, flightSource);
-        values.put(FLIGHT_COLUMN_REF_NO, refNo);
+        values.put(COLUMN_FLIGHT_NO, flightNo);
+        values.put(COLUMN_AIRCRAFT, aircraft);
+        values.put(COLUMN_AIRCRAFT_TYPE, aircraftType);
+        values.put(COLUMN_DESTINATION, destination);
+        values.put(COLUMN_AIRLINE_CODE, airlineCode);
+        values.put(COLUMN_FLIGHT_TIME, flightTime);
+        values.put(COLUMN_SOURCE, flightSource);
+        values.put(COLUMN_REF_NO, refNo);
 
         long result = db.insert(TABLE_NAME_FLIGHT_INFO, null, values);
+        if (result == -1) {
+            Toast.makeText(context, context.getString(R.string.error_msg), Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            Toast.makeText(context, context.getString(R.string.success_msg), Toast.LENGTH_SHORT).show();
+            return true;
+        }
+    }
+
+    public boolean addTicketInfo(int ticketNo, String ticketType, int refNo, int price, String status) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(COLUMN_TICKET_NO, ticketNo);
+        values.put(COLUMN_TICKET_TYPE, ticketType);
+        values.put(COLUMN_REF_NO, refNo);
+        values.put(COLUMN_TICKET_PRICE, price);
+        values.put(COLUMN_TICKET_STATUS, status);
+
+        long result = db.insert(TABLE_NAME_TICKET_INFO, null, values);
         if (result == -1) {
             Toast.makeText(context, context.getString(R.string.error_msg), Toast.LENGTH_SHORT).show();
             return false;
